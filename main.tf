35ae172e107d5b8caa2ffeddb239b3de0df1e6e9
sagar <sagar.b@phynart.com>
2023-03-06 15:37:56 +0530

    seperated out output file

--- main.tf
+++ main.tf
@@ -129,31 +129,3 @@ resource "aws_instance" "web_instance" {
     "Name" : var.ec2_tag_name
   }
 }
-
-output "vpc_id" {
-  value = aws_vpc.dev-vpc.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.dev-public-subnet.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.dev-private-subnet.id
-}
-
-output "igw_id" {
-  value = aws_internet_gateway.igw.id
-}
-
-output "route_table_id" {
-  value = aws_route_table.route_table.id
-}
-
-output "aws_security_group_id" {
-  value = aws_security_group.dev-sg.id  
-}
-
-output "aws_instance_id" {
-  value = aws_instance.web_instance.id
-}
