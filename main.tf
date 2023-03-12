91190d7f6b341d84530868d2bd6b1c21bbd33568
sagar <sagar.b@phynart.com>
2023-03-12 16:54:23 +0530

    seperated variables file

--- main.tf
+++ main.tf
@@ -7,33 +7,6 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-variable "access_key" {}
-variable "secret_key" {}
-
-variable "region" {
-  description = "this to be region"
-}
-variable "vpc_cidr_block" {}
-variable "vpc_tag_name" {}
-variable "availability_zone" {}
-
-variable "public_subnet_cidr_block" {}
-variable "public_subnet_tag_name" {}
-
-variable "private_subnet_cidr_block" {}
-variable "private_subnet_tag_name" {}
-
-variable "internet_gateway_tag_name" {}
-
-variable "route_table_tag_name" {}
-
-variable "security_group_name" {}
-
-variable "ec2_instance_ami" {}
-variable "ec2_instance_type" {}
-variable "ec2_key_name" {}
-variable "ec2_tag_name" {}
-
 resource "aws_vpc" "dev-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
