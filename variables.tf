fdc082e3859ff755a61b57e81dad74d38835d700
sagar <sagar.b@phynart.com>
2023-04-23 00:48:37 +0530

    added sensitive attibute to variables

--- variables.tf
+++ variables.tf
@@ -1,26 +1,65 @@
-variable "access_key" {}
-variable "secret_key" {}
+variable "access_key" {
+  type = string
+  sensitive = true
+}
+variable "secret_key" {
+  type = string
+  sensitive = true
+}
 
 variable "region" {
+  type = string
   description = "this to be region"
 }
-variable "vpc_cidr_block" {}
-variable "vpc_tag_name" {}
-variable "availability_zone" {}
+variable "vpc_cidr_block" {
+  type = string
+}
+variable "vpc_tag_name" {
+  type = string
+}
+variable "availability_zone" {
+  type = string
+  default = "ap-south-1"
+}
 
-variable "public_subnet_cidr_block" {}
-variable "public_subnet_tag_name" {}
+variable "public_subnet_cidr_block" {
+  type = string
+}
+variable "public_subnet_tag_name" {
+  type = string
+}
 
-variable "private_subnet_cidr_block" {}
-variable "private_subnet_tag_name" {}
+variable "private_subnet_cidr_block" {
+  type = string
+}
+variable "private_subnet_tag_name" {
+  type = string
+}
 
-variable "internet_gateway_tag_name" {}
+variable "internet_gateway_tag_name" {
+  type = string
+}
 
-variable "route_table_tag_name" {}
+variable "route_table_tag_name" {
+  type = string
+}
 
-variable "security_group_name" {}
+variable "security_group_name" {
+  type = string
+}
 
-variable "ec2_instance_ami" {}
-variable "ec2_instance_type" {}
-variable "ec2_key_name" {}
-variable "ec2_tag_name" {}
+variable "ec2_instance_ami" {
+  type = string
+}
+
+variable "ec2_instance_type" {
+  type = string
+}
+
+variable "ec2_key_name" {
+  type = string
+}
+
+variable "ec2_tag_name" {
+  type = string
+}
